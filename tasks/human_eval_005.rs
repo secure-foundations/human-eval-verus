7d0f9020077df18e42d369b08f1c8c851acf3d5c
Bryan Parno <parno@cmu.edu>
2024-07-18 22:03:07 -0400

    Complete the spec and verified implementation of Task 005

--- tasks/human_eval_005.rs
+++ tasks/human_eval_005.rs
@@ -5,11 +5,119 @@ HumanEval/5
 /*
 ### VERUS BEGIN
 */
+use vstd::assert_seqs_equal;
 use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+/// Specification for inserting a number 'delimeter' between every two consecutive elements
+/// of input sequence `numbers'
+pub open spec fn intersperse_spec(numbers: Seq<u64>, delimeter: u64) -> Seq<u64>
+    decreases numbers.len(),
+{
+    if numbers.len() <= 1 {
+        numbers
+    } else {
+        intersperse_spec(numbers.drop_last(), delimeter) + seq![delimeter, numbers.last()]
+    }
+}
+
+// We use these two functions to provide valid triggers for the quantifiers in intersperse_quantified
+spec fn even(i: int) -> int {
+    2 * i
+}
+
+spec fn odd(i: int) -> int {
+    2 * i + 1
+}
+
+// This quantified formulation of intersperse is easier to reason about in the implementation's loop
+spec fn intersperse_quantified(numbers: Seq<u64>, delimeter: u64, interspersed: Seq<u64>) -> bool {
+    (if numbers.len() == 0 {
+        interspersed.len() == 0
+    } else {
+        interspersed.len() == 2 * numbers.len() - 1
+    }) && (forall|i: int| 0 <= i < numbers.len() ==> #[trigger] interspersed[even(i)] == numbers[i])
+        && (forall|i: int|
+        0 <= i < numbers.len() - 1 ==> #[trigger] interspersed[odd(i)] == delimeter)
+}
+
+proof fn intersperse_spec_len(numbers: Seq<u64>, delimeter: u64)
+    ensures
+        numbers.len() > 0 ==> intersperse_spec(numbers, delimeter).len() == 2 * numbers.len() - 1,
+    decreases numbers.len(),
+{
+    if numbers.len() > 0 {
+        intersperse_spec_len(numbers.drop_last(), delimeter);
+    }
+}
+
+// Show that the two descriptions of intersperse are equivalent
+proof fn intersperse_quantified_is_spec(numbers: Seq<u64>, delimeter: u64, interspersed: Seq<u64>)
+    requires
+        intersperse_quantified(numbers, delimeter, interspersed),
+    ensures
+        interspersed =~= intersperse_spec(numbers, delimeter),
+    decreases numbers.len(),
+{
+    let is = intersperse_spec(numbers, delimeter);
+    if numbers.len() == 0 {
+    } else if numbers.len() == 1 {
+        assert(interspersed.len() == 1);
+        assert(interspersed[even(0)] == numbers[0]);
+    } else {
+        intersperse_quantified_is_spec(
+            numbers.drop_last(),
+            delimeter,
+            interspersed.take(interspersed.len() - 2),
+        );
+        intersperse_spec_len(numbers, delimeter);
+        assert_seqs_equal!(is == interspersed, i => {
+            if i < is.len() - 2 {
+            } else {
+                if i % 2 == 0 {
+                    assert(is[i] == numbers.last());
+                    assert(interspersed[even(i/2)] == numbers[i / 2]);
+                    assert(i / 2 == numbers.len() - 1);
+                } else {
+                    assert(is[i] == delimeter);
+                    assert(interspersed[odd((i-1)/2)] == delimeter);
+                }
+            }
+        });
+    }
+}
+
+/// Implementation of intersperse
+pub fn intersperse(numbers: Vec<u64>, delimeter: u64) -> (result: Vec<u64>)
+    ensures
+        result@ == intersperse_spec(numbers@, delimeter),
+{
+    if numbers.len() <= 1 {
+        numbers
+    } else {
+        let mut result = Vec::new();
+        let mut index = 0;
+        while index < numbers.len() - 1
+            invariant
+                numbers.len() > 1,
+                0 <= index < numbers.len(),
+                result.len() == 2 * index,
+                forall|i: int| 0 <= i < index ==> #[trigger] result[even(i)] == numbers[i],
+                forall|i: int| 0 <= i < index ==> #[trigger] result[odd(i)] == delimeter,
+        {
+            result.push(numbers[index]);
+            result.push(delimeter);
+            index += 1;
+            //assert(numbers@.subrange(0, index as int).drop_last() =~= numbers@.subrange(0, index as int - 1));
+        }
+        result.push(numbers[numbers.len() - 1]);
+        proof {
+            intersperse_quantified_is_spec(numbers@, delimeter, result@);
+        }
+        result
+    }
+}
 
 } // verus!
 fn main() {}
