bbe5c9e8dfd4092ac01a7e33d34457935a68f394
Edwin Fernando <ef322@ic.ac.uk>
2024-09-12 14:52:43 +0530

    complete is_cube excpet for lemma that cubes are increasing

--- tasks/human_eval_077.rs
+++ tasks/human_eval_077.rs
@@ -5,11 +5,119 @@ HumanEval/77
 /*
 ### VERUS BEGIN
 */
+use vstd::arithmetic::mul::*;
 use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+fn is_cube(x: i32) -> (ret: bool)
+    requires
+        x != i32::MIN,  // avoid overflow: -(i32::MIN) == (i32::MAX) + 1
+
+    ensures
+        ret <==> exists|i: int| 0 <= i && spec_abs(x as int) == #[trigger] (i * i * i),
+{
+    let x_abs = ex_abs(x);
+
+    // dealing with cases where the the cube is not greater than the number
+    if x_abs == 0 {
+        assert(spec_abs(x as int) == 0 * 0 * 0);
+        return true;
+    } else if (x_abs == 1) {
+        assert(spec_abs(x as int) == 1 * 1 * 1);
+        return true;
+    }
+    assert(-1 > x || x > 1);
+    let mut i = 2;
+    while i < x_abs
+        invariant
+            forall|j: int| 2 <= j < i ==> spec_abs(x as int) != #[trigger] (j * j * j),
+            2 <= i <= spec_abs(x as int) == x_abs,
+    {
+        let prod = checked_cube(i);
+        if prod.is_some() && prod.unwrap() == ex_abs(x) {
+            return true;
+        }
+        i += 1;
+    }
+    assert(forall|j: int| 2 <= j ==> spec_abs(x as int) != #[trigger] (j * j * j)) by {
+        assert(forall|j: int| 2 <= j < i ==> spec_abs(x as int) != #[trigger] (j * j * j));
+
+        assert(forall|j: int| i <= j ==> spec_abs(x as int) < #[trigger] (j * j * j)) by {
+            assert(spec_abs(x as int) < #[trigger] (i * i * i)) by {
+                broadcast use group_mul_properties;
+
+                assert(i <= i * i <= i * i * i);
+            }
+            lemma_cube_increases();
+            assert(forall|j: int| i <= j ==> (i * i * i) <= #[trigger] (j * j * j));
+        }
+    }
+    false
+}
+
+fn checked_cube(x: i32) -> (ret: Option<i32>)
+    requires
+        x >= 0,
+    ensures
+        ret.is_some() ==> ret.unwrap() == x * x * x,
+        ret.is_none() ==> x * x * x > i32::MAX,
+{
+    //x == 0 done separately to invoke lemma_mul_increases which requires x > 0
+    if x == 0 {
+        return Some(0);
+    }
+    let sqr = x.checked_mul(x);
+    if sqr.is_some() {
+        let cube = sqr.unwrap().checked_mul(x);
+        if cube.is_some() {
+            let ans = cube.unwrap();
+            assert(ans == x * x * x);
+            Some(ans)
+        } else {
+            assert(x * x * x > i32::MAX);
+            None
+        }
+    } else {
+        assert(x > 0);
+        assert(x * x > i32::MAX);
+        proof {
+            lemma_mul_increases(x as int, x * x);
+        }
+        assert(x * x * x >= x * x);
+        None
+    }
+}
+
+spec fn spec_abs(x: int) -> int {
+    if x < 0 {
+        -x
+    } else {
+        x
+    }
+}
+
+fn ex_abs(x: i32) -> (ret: i32)
+    requires
+        x != i32::MIN,  // avoid overflow: -(i32::MIN) == (i32::MAX) + 1
+
+    ensures
+        ret == spec_abs(x as int),
+{
+    if x < 0 {
+        -x
+    } else {
+        x
+    }
+}
+
+proof fn lemma_cube_increases()
+    ensures
+        forall|i: int, j: int| 0 <= i <= j ==> #[trigger] (i * i * i) <= #[trigger] (j * j * j),
+{
+    admit();
+    // lemma_mul_induction_auto(x, |u: int| 1 < u ==> u * u * u <= (u + 1) * (u + 1) * (u + 1));
+}
 
 } // verus!
 fn main() {}
