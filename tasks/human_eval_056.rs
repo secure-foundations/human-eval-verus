165fabace4a4eee1b9faa0c63e632d70d15291c4
edwin1729 <112767451+edwin1729@users.noreply.github.com>
2024-09-14 06:05:07 +0530

    wrote some programs (#9)
    @edwin1729 completed nine tasks, plus two solutions for an additional task.

--- tasks/human_eval_056.rs
+++ tasks/human_eval_056.rs
@@ -9,7 +9,58 @@ use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+// say the return value is (x, b), x = number of '<' - number of '>', b = whether x ever dipped below 0
+spec fn spec_bracketing_helper(brackets: Seq<char>) -> (int, bool) {
+    brackets.fold_left(
+        (0, true),
+        |p: (int, bool), c|
+            {
+                let (x, b) = p;
+                match (c) {
+                    '<' => (x + 1, b),
+                    '>' => (x - 1, b && x - 1 >= 0),
+                    _ => (x, b),
+                }
+            },
+    )
+}
+
+spec fn spec_bracketing(brackets: Seq<char>) -> bool {
+    let p = spec_bracketing_helper(brackets);
+    p.1 && p.0 == 0
+}
+
+fn correct_bracketing(brackets: &str) -> (ret: bool)
+    requires
+        brackets@.len() <= i32::MAX,
+        -brackets@.len() >= i32::MIN,
+    ensures
+        ret <==> spec_bracketing(brackets@),
+{
+    let mut i = 0;
+    let mut b = true;
+    let mut stack_size: i32 = 0;
+
+    while i < brackets.unicode_len()
+        invariant
+            (stack_size as int, b) == spec_bracketing_helper(brackets@.subrange(0, i as int)),
+            stack_size <= i <= brackets@.len() <= i32::MAX,
+            stack_size >= -i >= -brackets@.len() >= i32::MIN,
+    {
+        let c = brackets.get_char(i);
+        let ghost prev = spec_bracketing_helper(brackets@.subrange(0, i as int));
+        if (c == '<') {
+            stack_size += 1;
+        } else if (c == '>') {
+            b = b && stack_size > 0;
+            stack_size -= 1;
+        }
+        assert(brackets@.subrange(0, i + 1 as int).drop_last() =~= brackets@.subrange(0, i as int));
+        i += 1;
+    }
+    assert(brackets@ =~= brackets@.subrange(0, i as int));
+    b && stack_size == 0
+}
 
 } // verus!
 fn main() {}
