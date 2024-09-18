d96a82b98872e26b5008e9a25941d7dfe45d13c5
Edwin Fernando <ef322@ic.ac.uk>
2024-09-18 17:45:09 +0530

    fix lint

--- tasks/human_eval_070.rs
+++ tasks/human_eval_070.rs
@@ -5,67 +5,69 @@ HumanEval/70
 /*
 ### VERUS BEGIN
 */
+use vstd::calc;
 use vstd::prelude::*;
 use vstd::seq_lib::lemma_multiset_commutative;
-use vstd::calc;
 
 verus! {
 
 proof fn swap_preserves_multiset_helper(s: Seq<i32>, i: int, j: int)
-    requires 
+    requires
         0 <= i < j < s.len(),
     ensures
-        (s.take(j+1)).to_multiset() =~= s.take(i).to_multiset().add(s.subrange(i + 1, j).to_multiset()).insert(s.index(j)).insert(s.index(i)),
+        (s.take(j + 1)).to_multiset() =~= s.take(i).to_multiset().add(
+            s.subrange(i + 1, j).to_multiset(),
+        ).insert(s.index(j)).insert(s.index(i)),
 {
     let fst = s.take(i);
     let snd = s.subrange(i + 1, j);
 
-    assert((s.take(j+1)).to_multiset() =~= fst.to_multiset().insert(s.index(i)).add(snd.to_multiset().insert(s.index(j)))) by {
-        assert(s.take(i+1).to_multiset() =~= fst.to_multiset().insert(s.index(i))) by {
+    assert((s.take(j + 1)).to_multiset() =~= fst.to_multiset().insert(s.index(i)).add(
+        snd.to_multiset().insert(s.index(j)),
+    )) by {
+        assert(s.take(i + 1).to_multiset() =~= fst.to_multiset().insert(s.index(i))) by {
             fst.to_multiset_ensures();
-            assert(fst.push(s.index(i)) =~= s.take(i+1));
+            assert(fst.push(s.index(i)) =~= s.take(i + 1));
         }
-        assert(s.subrange(i+1, j+1).to_multiset() =~= snd.to_multiset().insert(s.index(j))) by {
+        assert(s.subrange(i + 1, j + 1).to_multiset() =~= snd.to_multiset().insert(s.index(j))) by {
             snd.to_multiset_ensures();
-            assert(snd.push(s.index(j)) =~= s.subrange(i+1, j+1));
+            assert(snd.push(s.index(j)) =~= s.subrange(i + 1, j + 1));
         }
-
-        lemma_multiset_commutative(s.take(i+1), s.subrange(i+1, j+1));
-        assert (s.take(i+1) + s.subrange(i+1, j+1) =~= s.take(j+1) );
+        lemma_multiset_commutative(s.take(i + 1), s.subrange(i + 1, j + 1));
+        assert(s.take(i + 1) + s.subrange(i + 1, j + 1) =~= s.take(j + 1));
     }
 }
 
 // Helper lemma to prove that swapping two elements doesn't change the multiset
 proof fn swap_preserves_multiset(s1: Seq<i32>, s2: Seq<i32>, i: int, j: int)
-    requires 
+    requires
         0 <= i < j < s1.len() == s2.len(),
         forall|x: int| 0 <= x < s1.len() && x != i && x != j ==> s1.index(x) == s2.index(x),
         s1.index(i) == s2.index(j),
         s1.index(j) == s2.index(i),
-
     ensures
         s1.to_multiset() == s2.to_multiset(),
 {
     calc! {
         (==)
         s1.to_multiset(); {
-            lemma_multiset_commutative(s1.take(j+1), s1.skip(j+1));
-            assert(s1 =~= s1.take(j+1) + s1.skip(j+1));
+            lemma_multiset_commutative(s1.take(j + 1), s1.skip(j + 1));
+            assert(s1 =~= s1.take(j + 1) + s1.skip(j + 1));
         }
-        s1.take(j+1).to_multiset().add(s1.skip(j+1).to_multiset()); {
-            assert(s1.take(j+1).to_multiset() =~= s2.take(j+1).to_multiset()) by {
+        s1.take(j + 1).to_multiset().add(s1.skip(j + 1).to_multiset()); {
+            assert(s1.take(j + 1).to_multiset() =~= s2.take(j + 1).to_multiset()) by {
                 assert(s1.take(i) == s2.take(i));
                 assert(s1.subrange(i + 1, j) =~= (s2.subrange(i + 1, j)));
                 swap_preserves_multiset_helper(s1, i, j);
                 swap_preserves_multiset_helper(s2, i, j);
             }
-            assert (s1.skip(j+1).to_multiset() =~= s2.skip(j+1).to_multiset()) by {
+            assert(s1.skip(j + 1).to_multiset() =~= s2.skip(j + 1).to_multiset()) by {
                 assert(s1.skip(j + 1) =~= s2.skip(j + 1));
             }
         }
-        s2.take(j+1).to_multiset().add(s2.skip(j+1).to_multiset()); {
-            lemma_multiset_commutative(s2.take(j+1), s2.skip(j+1));
-            assert(s2 =~= s2.take(j+1) + s2.skip(j+1));
+        s2.take(j + 1).to_multiset().add(s2.skip(j + 1).to_multiset()); {
+            lemma_multiset_commutative(s2.take(j + 1), s2.skip(j + 1));
+            assert(s2 =~= s2.take(j + 1) + s2.skip(j + 1));
         }
         s2.to_multiset();
     }
