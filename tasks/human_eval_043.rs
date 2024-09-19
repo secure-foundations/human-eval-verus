f2012ef7578b482dd7ad3cde2686fbc909b69436
WeetHet <stas.ale66@gmail.com>
2024-09-19 09:56:40 +0300

    Relax restrictions a bit

--- tasks/human_eval_043.rs
+++ tasks/human_eval_043.rs
@@ -14,8 +14,8 @@ fn pairs_sum_to_zero(nums: &[i32], target: i32) -> (found: bool)
     requires
         nums.len() >= 2,
         forall|i: int, j: int|
-            0 <= i < nums.len() && 0 <= j < nums.len() ==> nums[i] + nums[j] <= i32::MAX && nums[i]
-                + nums[j] >= i32::MIN,
+            0 <= i < j < nums.len() ==> nums[i] + nums[j] <= i32::MAX && nums[i] + nums[j]
+                >= i32::MIN,
     ensures
         found <==> exists|i: int, j: int| 0 <= i < j < nums.len() && nums[i] + nums[j] == target,
 {
