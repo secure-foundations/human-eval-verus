1bb4040c66351813199b661c878ec878d597c2f7
WeetHet <stas.ale66@gmail.com>
2024-09-19 09:44:55 +0300

    Complete 035

--- tasks/human_eval_035.rs
+++ tasks/human_eval_035.rs
@@ -9,7 +9,25 @@ use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+fn max_element(a: &Vec<i32>) -> (max: i32)
+    requires
+        a.len() > 0,
+    ensures
+        forall|i: int| 0 <= i < a.len() ==> a[i] <= max,
+        exists|i: int| 0 <= i < a.len() && a[i] == max,
+{
+    let mut max = a[0];
+    for i in 1..a.len()
+        invariant
+            forall|j: int| 0 <= j < i ==> a[j] <= max,
+            exists|j: int| 0 <= j < i && a[j] == max,
+    {
+        if a[i] > max {
+            max = a[i];
+        }
+    }
+    max
+}
 
 } // verus!
 fn main() {}
