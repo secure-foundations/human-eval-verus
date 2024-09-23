f06591e1a3a205d33f7ff16574ce3ec0efd84127
WeetHet <stas.ale66@gmail.com>
2024-09-23 12:44:06 +0300

    Complete 100

--- tasks/human_eval_100.rs
+++ tasks/human_eval_100.rs
@@ -9,7 +9,30 @@ use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+fn make_a_pile(n: usize) -> (pile: Vec<usize>)
+    requires
+        n + (2 * n) <= usize::MAX,
+    ensures
+        pile.len() == n,
+        n > 0 ==> pile[0] == n,
+        forall|i: int| #![trigger pile[i]] 1 <= i < n ==> pile[i] == pile[i - 1] + 2,
+{
+    if n == 0 {
+        return vec![];
+    }
+    let mut pile = vec![n];
+    for i in 1..n
+        invariant
+            pile.len() == i,
+            pile[i - 1] + (2 * (n - i)) <= usize::MAX,
+            forall|j: int| #![trigger pile[j]] 1 <= j < i ==> pile[j] == pile[j - 1] + 2,
+            n > 0 ==> pile[0] == n,
+    {
+        let prev = pile[i - 1];
+        pile.push(prev + 2);
+    }
+    pile
+}
 
 } // verus!
 fn main() {}
