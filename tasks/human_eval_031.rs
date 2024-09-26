29e63bd28884a77a9f38f148fe39cb72c606c328
Elanor Tang <estang@GS24452.SP.CS.CMU.EDU>
2024-09-26 16:47:34 -0400

    completed task 31

--- tasks/human_eval_031.rs
+++ tasks/human_eval_031.rs
@@ -9,10 +9,77 @@ use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+// specification
+pub closed spec fn prime_helper(n: nat, k: nat) -> bool
+    recommends
+        2 <= k <= n,
+    decreases n - k,
+{
+    if k < 2 || k > n {
+        false  //arbitrarily chosen return value, should have 2 <= k <= n
+
+    } else if n >= k >= n - 1 {
+        true
+    } else if n % k == 0 {
+        false
+    } else {
+        prime_helper(n, k + 1)
+    }
+}
+
+pub open spec fn is_prime(n: nat) -> bool {
+    if n < 2 {
+        false
+    } else {
+        prime_helper(n, 2)
+    }
+}
+
+proof fn sanity_check() {
+    assert(is_prime(6) == false) by (compute);
+    assert(is_prime(101) == true) by (compute);
+    assert(is_prime(11) == true) by (compute);
+    assert(is_prime(13441) == true) by (compute);
+    assert(is_prime(61) == true) by (compute);
+    assert(is_prime(4) == false) by (compute);
+    assert(is_prime(1) == false) by (compute);
+}
+
+// implementation
+fn is_prime_impl(n: u8) -> (res: bool)
+    ensures
+        res == is_prime(n as nat),
+{
+    if n < 2 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    let mut k = n - 1;
+    let mut res = true;
+    while (k > 2)
+        invariant
+            2 <= k <= n - 1,
+            res == prime_helper(n as nat, k as nat),
+    {
+        k = k - 1;
+        if n % k == 0 {
+            res = false;
+        }
+    }
+    return res;
+}
 
 } // verus!
-fn main() {}
+fn main() {
+    print!("6 is prime? {}\n", is_prime_impl(6));
+    print!("101 is prime? {}\n", is_prime_impl(101));
+    print!("11 is prime? {}\n", is_prime_impl(11));
+    print!("61 is prime? {}\n", is_prime_impl(61));
+    print!("4 is prime? {}\n", is_prime_impl(4));
+    print!("1 is prime? {}\n", is_prime_impl(1));
+}
 
 /*
 ### VERUS END
