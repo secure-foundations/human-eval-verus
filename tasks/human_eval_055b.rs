0555f44088beab3de7525413170017f6b2b546d4
Bryan Parno <parno@cmu.edu>
2024-09-28 13:35:47 -0400

    Strengthen the spec for 55b

--- tasks/human_eval_055b.rs
+++ tasks/human_eval_055b.rs
@@ -10,6 +10,8 @@ use vstd::prelude::*;
 verus! {
 
 // O(n) non-recursive solution using same spec as 55a
+
+#[verifier::memoize]
 spec fn spec_fib(n: nat) -> nat
     decreases n,
 {
@@ -22,9 +24,29 @@ spec fn spec_fib(n: nat) -> nat
     }
 }
 
+proof fn lemma_fib_monotonic(i:nat, j:nat)
+    requires
+        i <= j
+    ensures
+        spec_fib(i) <= spec_fib(j),
+    decreases j - i,
+{
+    if (i < 2 && j < 2) || i == j {
+    } else if i == j - 1 {
+        reveal_with_fuel(spec_fib, 2);
+        lemma_fib_monotonic(i, (j - 1) as nat);
+    } else {
+        lemma_fib_monotonic(i, (j - 1) as nat);
+        lemma_fib_monotonic(i, (j - 2) as nat);
+    }
+}
+
 fn fib(n: u32) -> (ret: Option<u32>)
     ensures
-        ret.is_some() ==> ret.unwrap() == spec_fib(n as nat),
+        match ret {
+            None => spec_fib(n as nat) > u32::MAX,
+            Some(f) => f == spec_fib(n as nat)
+        },
 {
     if n == 0 {
         return Some(0);
@@ -32,17 +54,31 @@ fn fib(n: u32) -> (ret: Option<u32>)
     if n == 1 {
         return Some(1);
     }
+    if n > 47 {
+        proof {
+            assert(spec_fib(48) > u32::MAX) by (compute_only);
+            lemma_fib_monotonic(48, n as nat);
+        }
+        return None;
+    }
     let mut a: u32 = 0;
     let mut b: u32 = 1;
     let mut i: u32 = 2;
 
     for i in 1..n
         invariant
-            1 <= i as int <= n,
+            1 <= i as int <= n <= 47,
             a as int == spec_fib((i - 1) as nat),
             b as int == spec_fib(i as nat),
     {
-        let sum = a.checked_add(b)?;
+        proof {
+            // Prove that that n1 + n2 won't overflow
+            assert(spec_fib(47) < u32::MAX) by (compute_only);
+            lemma_fib_monotonic(i as nat, 47);
+            lemma_fib_monotonic((i - 1) as nat, 47);
+            lemma_fib_monotonic((i + 1) as nat, 47);
+        }
+        let sum = a + b;
         a = b;
         b = sum;
     }
