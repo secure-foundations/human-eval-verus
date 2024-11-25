5aeba80280a9f9d6e5982189fade00a9b6fbbc71
Bryan Parno <parno@cmu.edu>
2024-11-25 16:45:40 -0500

    Clean up main.js

--- .github/actions/latest-github-release/dist/index.js
+++ .github/actions/latest-github-release/dist/index.js
@@ -31165,51 +31165,26 @@ async function run() {
             repo: repo,
             });
         releases = releases.data;
-        console.log("releases");
-        console.log(releases);
+        //console.log("releases");
+        //console.log(releases);
         var latest_release = releases[0];
         var release = latest_release;
-        // // var release = await octokit.rest.repos.getLatestRelease({
-        // //     owner: owner,
-        // //     repo: repo,
-        // //     });
-        // //release = release.data;
-
-        // var release = await octokit.rest.repos.getLatestRelease({
-        //      owner: owner,
-        //      repo: repo,
-        //      });
-        // release = release.data;
         var assets = await octokit.rest.repos.listReleaseAssets({
             owner: owner,
             repo: repo,
             release_id: release.id,
         });
-        console.log("assets");
-        console.log(assets);
-        console.log(assets.length);
+        //console.log("assets");
+        //console.log(assets);
 
         for (const asset of assets.data) {
-            console.log(asset);
+            console.log(asset.name);
             if (asset.name.indexOf(platform) >= 0) {
                 core.setOutput('url', asset.browser_download_url);
                 break;
             }
         }
-        // if (excludes.includes('prerelease')) {
-        //     releases = releases.filter(x => x.prerelease != true);
-        // }
-        // if (excludes.includes('draft')) {
-        //     releases = releases.filter(x => x.draft != true);
-        // }
-        // if (releases.length) {
-        //     core.setOutput('release', releases[0].tag_name);
-        //     core.setOutput('url', releases[0].url);
-        //     //core.setOutput('id', String(releases[0].id));
-        //     //core.setOutput('description', String(releases[0].body));
-        // } else {
-        //     core.setFailed("No valid releases");
-        // }
+        console.log("error: failed to find a matching asset");
     }
     catch (error) {
         core.setFailed(error.message);
