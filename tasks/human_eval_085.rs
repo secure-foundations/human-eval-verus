89a76ce258c2d7bc721d88b3e527a01bdf73deb5
zero-to-nat <neamtu@cs.washington.edu>
2024-11-26 12:24:22 -0800

    first attempt at task 85

--- tasks/human_eval_085.rs
+++ tasks/human_eval_085.rs
@@ -9,7 +9,55 @@ use vstd::prelude::*;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+/// Returns x if x is odd, else returns 0.
+pub open spec fn odd_or_zero(x: u32) -> u32
+{
+    if x % 2 == 0 { x } else { 0 }
+}
+
+/// Sum of all even elements at odd indices in the list.
+pub open spec fn add_odd_evens(lst: Seq<u32>) -> int
+    decreases lst.len()
+{
+    if (lst.len() < 2) {
+        0
+    } else {
+        odd_or_zero(lst[1]) + add_odd_evens(lst.skip(2))
+    }
+}
+
+/// Implementation:
+/// Needed to change from (signed) integer to unsigned integer: Verus doesn't yet support % for signed integers.
+/// Original definition is underspecified when list has no applicable elements: assume that the desired return value is 0 for lists with no even integers at odd indices.
+fn add(lst: Vec<u32>) -> (sum: u64)
+    requires 
+        0 < lst.len() < u32::MAX
+    ensures
+        sum == add_odd_evens(lst@)
+{
+    let mut sum: u64 = 0;
+    let mut i = 1;
+    proof {
+        assert(lst@ =~= lst@.skip(0));
+    }
+    while (i < lst.len())
+        invariant
+            1 <= i <= lst.len() + 1,
+            0 < lst.len() < u32::MAX,
+            sum <= (u32::MAX) * i,
+            sum == add_odd_evens(lst@) - add_odd_evens(lst@.skip(i - 1 as int)),
+    {
+        if (lst[i] % 2 == 0) {
+            sum += lst[i] as u64;
+        }
+        proof {
+            // proves: add_odd_evens(lst@.skip(i - 1)) == odd_or_zero(lst[i]) + add_odd_evens(lst@.skip(i + 1))
+            assert(lst@.skip(i - 1 as int).skip(2) =~= lst@.skip(i + 1 as int));
+        }
+        i += 2;
+    }
+    return sum;
+}
 
 } // verus!
 fn main() {}
