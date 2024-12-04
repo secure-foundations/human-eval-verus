3e0a490bd33a5e0842460d4b63052ebc0538fcc0
zero-to-nat <neamtu@cs.washington.edu>
2024-12-03 17:20:04 -0800

    add comments and a couple todos

--- tasks/human_eval_087.rs
+++ tasks/human_eval_087.rs
@@ -10,34 +10,40 @@ use vstd::prelude::*;
 
 verus! {
 
+/// coords is sorted by row in ascending order.
 pub open spec fn row_sorted_asc(coords: Seq<(usize, usize)>) -> bool {
     forall|i: usize, j: usize|
         0 <= i < j < coords.len() ==> #[trigger] coords[i as int].0 <= #[trigger] coords[j as int].0
 }
 
-pub open spec fn col_sorted_desc(coords: Seq<(usize, usize)>) -> bool {
+/// In coords, the coordinates of same row are sorted by column in descending order.
+pub open spec fn row_col_sorted_desc(coords: Seq<(usize, usize)>) -> bool {
     forall|i: usize, j: usize|
         0 <= i < j < coords.len() && #[trigger] coords[i as int].0 == #[trigger] coords[j as int].0
             ==> coords[i as int].1 > coords[j as int].1
 }
 
+/// coords contains all coordinates matching x in lst within the given row and at or after the given column.
 pub open spec fn coords_complete_for_row_until_col(
     lst: Seq<Seq<i32>>,
     x: i32,
     coords: Seq<(usize, usize)>,
-    i: usize,
+    row: usize,
     col: usize,
 ) -> bool
     recommends
-        0 <= i < lst.len(),
+        0 <= row < lst.len(),
 {
     forall|j: usize|
-        #![trigger lst[i as int][j as int]]
-        0 <= j < lst[i as int].len() && col <= j && lst[i as int][j as int] == x ==> exists|k: int|
+        #![trigger lst[row as int][j as int]]
+        0 <= j < lst[row as int].len() && col <= j && lst[row as int][j as int] == x ==> exists|
+            k: int,
+        |
             #![trigger coords[k]]
-            0 <= k < coords.len() && coords[k] == (i, j)
+            0 <= k < coords.len() && coords[k] == (row, j)
 }
 
+/// coords contains all coordinates matching x in lst before the given row.
 pub open spec fn coords_complete_until_row(
     lst: Seq<Seq<i32>>,
     x: i32,
@@ -49,10 +55,12 @@ pub open spec fn coords_complete_until_row(
         0 <= i < lst.len() && i < row ==> coords_complete_for_row_until_col(lst, x, coords, i, 0)
 }
 
+/// coords contains all coordinates matching x in lst.
 pub open spec fn coords_complete(lst: Seq<Seq<i32>>, x: i32, coords: Seq<(usize, usize)>) -> bool {
     coords_complete_until_row(lst, x, coords, lst.len() as usize)
 }
 
+/// All points in coords represent occurrences of x in lst.
 pub open spec fn coords_sound(lst: Seq<Seq<i32>>, x: i32, coords: Seq<(usize, usize)>) -> bool {
     forall|i: usize, j: usize|
         #![trigger lst[i as int][j as int]]
@@ -61,6 +69,7 @@ pub open spec fn coords_sound(lst: Seq<Seq<i32>>, x: i32, coords: Seq<(usize, us
             ==> lst[i as int][j as int] == x
 }
 
+/// coords exactly matches all of the occurrences of x in lst.
 pub open spec fn coords_matches_lst(
     lst: Seq<Seq<i32>>,
     x: i32,
@@ -70,39 +79,42 @@ pub open spec fn coords_matches_lst(
     &&& coords_sound(lst, x, coords)
 }
 
+/// Returns lst as a Seq.
 pub open spec fn lst_seq_refl(lst: Vec<Vec<i32>>) -> Seq<Seq<i32>> {
     lst@.map_values(|v: Vec<i32>| v@)
 }
 
+/// Returns coords as a Seq.
 pub open spec fn coords_seq_refl(coords: Vec<(usize, usize)>) -> Seq<(usize, usize)> {
     coords@
 }
 
+/// Returns list of all points in lst whose value is x, sorted by row in ascending order and then by column in descending order.
 fn get_row(lst: Vec<Vec<i32>>, x: i32) -> (coords: Vec<(usize, usize)>)
     ensures
         coords_matches_lst(lst_seq_refl(lst), x, coords@),
         row_sorted_asc(coords@),
-        col_sorted_desc(coords@)
+        row_col_sorted_desc(coords@),
 {
     let mut coords: Vec<(usize, usize)> = Vec::new();
     let mut i = 0;
-    let ghost lst_seq: Seq<Seq<i32>> = lst_seq_refl(lst);
+    let ghost ghost_seq: Seq<Seq<i32>> = lst_seq_refl(lst);
     let ghost ghost_coords: Seq<(usize, usize)> = coords_seq_refl(coords);
     for i in 0..lst.len()
         invariant
-            lst_seq == lst_seq_refl(lst),
+            ghost_seq == lst_seq_refl(lst),
             ghost_coords == coords_seq_refl(coords),
             forall|k: int| 0 <= k < coords.len() ==> #[trigger] coords@[k].0 < i,
             row_sorted_asc(coords@),
-            col_sorted_desc(coords@),
-            coords_sound(lst_seq, x, coords@),
-            coords_complete_until_row(lst_seq, x, coords@, i),
+            row_col_sorted_desc(coords@),
+            coords_sound(ghost_seq, x, coords@),
+            coords_complete_until_row(ghost_seq, x, coords@, i),
     {
         let n = lst[i].len();
         // construct list of coordinates for row in descending column order
         for j in 0..n
             invariant
-                lst_seq == lst_seq_refl(lst),
+                ghost_seq == lst_seq_refl(lst),
                 ghost_coords == coords_seq_refl(coords),
                 0 <= i < lst.len(),
                 n == lst[i as int].len(),
@@ -111,21 +123,24 @@ fn get_row(lst: Vec<Vec<i32>>, x: i32) -> (coords: Vec<(usize, usize)>)
                     0 <= k < coords.len() && #[trigger] coords@[k].0 == i ==> coords@[k].1 > n - 1
                         - j,
                 row_sorted_asc(coords@),
-                col_sorted_desc(coords@),
-                coords_sound(lst_seq, x, coords@),
-                coords_complete_until_row(lst_seq, x, coords@, i),
-                coords_complete_for_row_until_col(lst_seq, x, coords@, i, (n - j) as usize),
+                row_col_sorted_desc(coords@),
+                coords_sound(ghost_seq, x, coords@),
+                coords_complete_until_row(ghost_seq, x, coords@, i),
+                coords_complete_for_row_until_col(ghost_seq, x, coords@, i, (n - j) as usize),
         {
             if (lst[i][n - 1 - j] == x) {
                 coords.push((i, n - 1 - j));
 
                 proof {
-                    assert(coords_complete_until_row(lst_seq, x, coords@, i)) by {
+                    // todo - is there a better way to do this?
+                    // seems inefficient to use a ghost variable to assert that the list has only mutated by adding an element to the end
+                    assert(coords_complete_until_row(ghost_seq, x, coords@, i)) by {
                         assert(coords@.drop_last() =~= ghost_coords);
                     }
 
+                    // same todo as above
                     assert(coords_complete_for_row_until_col(
-                        lst_seq,
+                        ghost_seq,
                         x,
                         coords@.drop_last(),
                         i,
@@ -137,13 +152,13 @@ fn get_row(lst: Vec<Vec<i32>>, x: i32) -> (coords: Vec<(usize, usize)>)
                     ghost_coords = ghost_coords.push((i, (n - 1 - j) as usize));
 
                     assert(coords_complete_for_row_until_col(
-                        lst_seq,
+                        ghost_seq,
                         x,
                         coords@,
                         i,
                         (n - j - 1) as usize,
                     )) by {
-                        assert(ghost_coords[ghost_coords.len() - 1] == (i, (n - j - 1) as usize));
+                        assert(coords[coords.len() - 1] == (i, (n - j - 1) as usize));
                     }
                 }
             }
