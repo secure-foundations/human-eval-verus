fd72a25856af209c91f6548e11114d14967d3ab4
Zhengyao Lin <zhengyao-lin@outlook.com>
2025-01-20 13:53:26 -0500

    Task 017

--- tasks/human_eval_017.rs
+++ tasks/human_eval_017.rs
@@ -11,8 +11,96 @@ verus! {
 
 // TODO: Put your solution (the specification, implementation, and proof) to the task here
 
+pub open spec fn spec_parse_music(s: Seq<char>) -> Option<Seq<int>>
+    decreases s.len()
+{
+    if s.len() == 0 {
+        Some(seq![])
+    } else if s.len() >= 2 && s[0] == 'o' && s[1] == '|' {
+        if let Some(rest) = spec_parse_music(s.skip(2)) {
+            Some(seq![2] + rest)
+        } else { None }
+    } else if s.len() >= 2 && s[0] == '.' && s[1] == '|' {
+        if let Some(rest) = spec_parse_music(s.skip(2)) {
+            Some(seq![1] + rest)
+        } else { None }
+    } else if s.len() != 0 && s[0] == 'o' {
+        if let Some(rest) = spec_parse_music(s.skip(1)) {
+            Some(seq![4] + rest)
+        } else { None }
+    } else if s.len() != 0 && s[0] == ' ' {
+        // Somewhat more lenient: allows multiple spaces between notes
+        spec_parse_music(s.drop_first())
+    } else {
+        None
+    }
+}
+
+pub fn parse_music(s: &str) -> (res: Option<Vec<u8>>)
+    ensures
+        res matches Some(res) ==> spec_parse_music(s@) == Some(res@.map_values(|b| b as int)),
+        res is None ==> spec_parse_music(s@) is None,
+{
+    let s_len = s.unicode_len();
+    let mut beats = vec![];
+    let mut i = 0;
+
+    assert(s@.skip(0) == s@);
+
+    while i < s_len
+        invariant
+            s_len == s@.len(),
+            0 <= i <= s_len,
+            spec_parse_music(s@) matches Some(all_beats) ==> {
+                &&& spec_parse_music(s@.skip(i as int)) matches Some(rest)
+                &&& all_beats =~= beats@.map_values(|b| b as int) + rest
+            },
+            spec_parse_music(s@.skip(i as int)) matches Some(rest)
+                ==> spec_parse_music(s@) matches Some(all_beats),
+            spec_parse_music(s@.skip(i as int)) is None ==>
+                spec_parse_music(s@) is None,
+
+        decreases s_len - i,
+    {
+        let c = s.get_char(i);
+
+        assert(s@.skip(i as int).skip(1) == s@.skip(i + 1));
+        assert(s@.len() >= i + 2 ==> s@.skip(i as int).skip(2) == s@.skip(i + 2));
+
+        if c == ' ' {
+            i += 1;
+            continue;
+        }
+
+        if i < s_len - 1 {
+            let c2 = s.get_char(i + 1);
+
+            if c == 'o' && c2 == '|' {
+                beats.push(2);
+                i += 2;
+                continue;
+            } else if c == '.' && c2 == '|' {
+                beats.push(1);
+                i += 2;
+                continue;
+            }
+        }
+
+        if c == 'o' {
+            beats.push(4);
+            i += 1;
+        } else {
+            return None;
+        }
+    }
+
+    Some(beats)
+}
+
 } // verus!
-fn main() {}
+fn main() {
+    // eprintln!("{:?}", parse_music("o o| .| o| o| .| .| .| .| o o"));
+}
 
 /*
 ### VERUS END
