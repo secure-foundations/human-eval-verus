751e8a941829f7f7cc40fe113b576ce6a5dbb0aa
ahuoguo <ahuoguo@gmail.com>
2025-02-04 17:09:36 +0100

    finish proof

--- tasks/human_eval_013.rs
+++ tasks/human_eval_013.rs
@@ -10,71 +10,189 @@ use vstd::prelude::*;
 // TODO: Put your solution (the specification, implementation, and proof) to the task here
 verus! {
 
+// This function is part of the specification
 pub open spec fn mul(a: nat, b: nat) -> nat {
     builtin::mul(a, b)
 }
 
-pub open spec fn divides(v: nat, d: nat) -> bool {
-    exists|k: nat| mul(d, k) == v
+// This function is also part of the specification
+pub open spec fn divides(factor: nat, candidate: nat) -> bool {
+    exists|k: nat| mul(factor, k) == candidate
 }
 
-pub open spec fn gcd_spec(a: nat, b: nat) -> nat
-    recommends
-        a > 0 && b > 0,
-    decreases a + b,
-    via gcd_spec_dec
+// This function is also part of the specification
+spec fn gcd(a: nat, b: nat) -> nat
+    decreases a, b,
 {
-    if a == b {
+    if a == 0 {
+        b
+    } else if b == 0 {
         a
-    } else if a < b {
-        gcd_spec(a, (b - a) as nat)
+    } else if a > b {
+        gcd(b, a % b)
     } else {
-        gcd_spec((a - b) as nat, b)
+        gcd(a, b % a)
     }
 }
 
-#[via_fn]
-proof fn gcd_spec_dec(a: nat, b: nat) {
-    // TODO, why >= here can't work
-    assume(a + b > a + ((b - a) as nat));
-}
-
-#[verifier::nonlinear]
-proof fn helper(a: nat, b: nat, k: nat)
-    requires
-        a > 0 && b > 0 && k > 0,
-        divides(k, a) && divides(k, b),
-        b >= a,
+fn compute_gcd(a: u64, b: u64) -> (g: u64)
     ensures
-        exists|m: nat, n: nat| a == mul(m, k) && b == mul(n, k) && m <= n,
+        g == gcd(a as nat, b as nat),
 {
-    let m_witness = choose|m: nat| #![trigger mul(m, k)] exists|k: nat| mul(m, k) == a;
-    assume(mul(m_witness, k) == a);
-    // TODO: triggers??
-    let n_witness = choose|n: nat| #![trigger mul(n, k)] exists|k: nat| mul(n, k) == b;
-    if m_witness <= n_witness {
-        assert(a == mul(m_witness, k) && b == mul(n_witness, k) && m_witness <= n_witness);
-
-        assert(exists|m: nat, n: nat| a == mul(m, k) && b == mul(n, k) && m <= n);
-    } else {
-        assume(false);
+    let mut a1 = a;
+    let mut b1 = b;
+    loop
+        invariant
+            gcd(a1 as nat, b1 as nat) == gcd(a as nat, b as nat),
+    {
+        if a1 == 0 {
+            return b1;
+        } else if b1 == 0 {
+            return a1;
+        } else if a1 > b1 {
+            let m = a1 % b1;
+            a1 = b1;
+            b1 = m;
+        } else {
+            b1 = b1 % a1;
+        }
     }
 }
 
-fn greatest_common_divisor(a: u32, b: u32) -> (result: u32)
-    requires
-        a > 0 && b > 0,
+// Helper function to prove gcd is a divisor of a and b
+proof fn gcd_devides(a: nat, b: nat)
     ensures
-        divides(result as nat, a as nat) && divides(result as nat, b as nat),
-        forall|x: nat| divides(x, a as nat) && divides(x, b as nat) ==> divides(x, result as nat),
+        divides(gcd(a, b), a),
+        divides(gcd(a, b), b),
+    decreases a, b,
 {
-    assume(false);
-    if a == b {
-        a
-    } else if a < b {
-        greatest_common_divisor(a, b - a)
+    if a == 0 {
+        assert(gcd(a, b) == b);
+        assert(divides(b, a)) by {
+            assert(mul(b, 0) == 0);
+        };
+        assert(divides(b, b)) by {
+            assert(mul(b, 1) == b);
+        };
+    } else if b == 0 {
+        assert(divides(a, a)) by {
+            assert(mul(a, 1) == a);
+        };
+        assert(divides(a, b)) by {
+            assert(mul(a, 0) == 0);
+        };
+    } else if a > b {
+        let m = a % b;
+        gcd_devides(b, m);
+        assert(divides(gcd(a, b), a)) by {
+            lemma_div_converse(a, b, gcd(b, m));
+        };
     } else {
-        greatest_common_divisor(a - b, b)
+        let m = b % a;
+        gcd_devides(a, m);
+        assert(divides(gcd(a, b), b)) by {
+            lemma_div_converse(b, a, gcd(a, m));
+        }
+
+    }
+}
+
+// if d divides a and d divides b, then d divides gcd(a, b)
+proof fn gcd_is_greatest(a: nat, b: nat)
+    ensures
+        forall|d: nat| divides(d, a) && divides(d, b) ==> #[trigger] divides(d, gcd(a, b)),
+    decreases a, b,
+{
+    assert forall|d: nat| divides(d, a) && divides(d, b) implies #[trigger] divides(
+        d,
+        gcd(a, b),
+    ) by {
+        if a == 0 {
+            assert(divides(d, gcd(a, b)));
+        } else if b == 0 {
+            assert(divides(d, gcd(a, b)));
+        } else if a > b {
+            let m = a % b;
+            gcd_is_greatest(b, m);
+            assert(divides(d, m)) by {
+                lemma_div(a, b, d);
+            };
+            assert(gcd(a, b) == gcd(b, m));
+            assert(divides(d, gcd(a, b)));
+        } else {
+            let m = b % a;
+            gcd_is_greatest(a, m);
+            assert(divides(d, m)) by {
+                lemma_div(b, a, d);
+            };
+            assert(gcd(a, b) == gcd(a, m));
+            assert(divides(d, gcd(a, b)));
+        }
+    }
+}
+
+#[verifier::spinoff_prover]
+proof fn lemma_div(a: nat, b: nat, d: nat)
+    requires
+        divides(d, a) && divides(d, b) && a >= b && a != 0 && b != 0,
+    ensures
+        divides(d, a % b),
+{
+    vstd::arithmetic::div_mod::lemma_fundamental_div_mod(a as int, b as int);
+    assert(a % b == a - b * (a / b));
+    assert(divides(d, a));
+    assert(divides(d, b * (a / b))) by {
+        let k = choose|k: nat| mul(d, k) == b;
+        let k1 = k * (a / b);
+        broadcast use vstd::arithmetic::mul::group_mul_properties;
+
+        assert(mul(d, k1) == b * (a / b));
+    };
+    assert(divides(d, a % b)) by {
+        let k1 = choose|k: nat| mul(d, k) == a;
+        let k2 = choose|k: nat| mul(d, k) == b * (a / b);
+        assert(a >= b * (a / b));
+        assert(k1 >= k2) by {
+            // note: is there a better way to proof by contradiction?
+            if (k1 < k2) {
+                // note: using broadcast fails here
+                vstd::arithmetic::mul::lemma_mul_strict_inequality(k1 as int, k2 as int, d as int);
+                assert(mul(k1, d) < mul(k2, d));
+            }
+        }
+        let k = k1 - k2;
+        // broadcast use vstd::arithmetic::mul::group_mul_properties; // cause trigger loop
+        assert(mul(d, k as nat) == mul(d, k1) - mul(d, k2)) by {
+            // broadcast use vstd::arithmetic::mul::lemma_mul_is_distributive_sub;
+            vstd::arithmetic::mul::lemma_mul_is_distributive_sub(d as int, k1 as int, k2 as int);
+        };
+    };
+}
+
+#[verifier::spinoff_prover]
+proof fn lemma_div_converse(a: nat, b: nat, d: nat)
+    requires
+        divides(d, b) && divides(d, a % b) && a >= b && a != 0 && b != 0,
+    ensures
+        divides(d, a),
+{
+    vstd::arithmetic::div_mod::lemma_fundamental_div_mod(a as int, b as int);
+    // assert(a % b == a - b * (a / b));
+    assert(divides(d, b));
+    assert(divides(d, b * (a / b))) by {
+        let k = choose|k: nat| mul(d, k) == b;
+        let k1 = k * (a / b);
+        broadcast use vstd::arithmetic::mul::group_mul_properties;
+
+        assert(mul(d, k1) == b * (a / b));
+    };
+    assert(divides(d, a)) by {
+        let k1 = choose|k: nat| mul(d, k) == a % b;
+        let k2 = choose|k: nat| mul(d, k) == b * (a / b);
+        let k = k1 + k2;
+        assert(mul(d, k) == mul(d, k1) + mul(d, k2)) by {
+            vstd::arithmetic::mul::lemma_mul_is_distributive_add(d as int, k1 as int, k2 as int);
+        };
     }
 }
 
