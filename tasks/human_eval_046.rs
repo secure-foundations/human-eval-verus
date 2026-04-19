62e2ed75f4d77dadd9fd5b495aafc1374cad5de4
Álvaro Silva <amfpsilva@hotmail.com>
2026-04-19 16:45:02 -0400

    Added solution to problem 46 (#40)

--- tasks/human_eval_046.rs
+++ tasks/human_eval_046.rs
@@ -5,14 +5,124 @@ HumanEval/46
 /*
 ### VERUS BEGIN
 */
-use vstd::prelude::*;
+use vstd::{invariant, prelude::*};
 
 verus! {
 
+proof fn lemma_fib4_n70_does_not_overflow()
+    ensures
+        fib4_spec(70) <= u64::MAX,
+{
+    // fib4_spec need to be memoize for computation to go through
+    assert(fib4_spec(70) <= u64::MAX) by (compute_only);
+}
+
+proof fn lemma_fib4_is_monotomic_after_inc_4(n: nat)
+    ensures
+        (n > 3) ==> (fib4_spec(n) <= fib4_spec(n + 1)),
+    decreases n,
+{
+    if (n <= 3) {
+    } else {
+        lemma_fib4_is_monotomic_after_inc_4((n - 1) as nat);
+        lemma_fib4_is_monotomic_after_inc_4((n - 2) as nat);
+        lemma_fib4_is_monotomic_after_inc_4((n - 3) as nat);
+        lemma_fib4_is_monotomic_after_inc_4((n - 4) as nat);
+    }
+
+}
+
+#[verifier::memoize]
+spec fn fib4_spec(n: nat) -> (out: nat)
+    decreases n,
+{
+    match n {
+        x if x <= 0 => 0,
+        x if x == 1 => 0,
+        x if x == 2 => 2,
+        x if x == 3 => 0,
+        _ => fib4_spec((n - 1) as nat) + fib4_spec((n - 2) as nat) + fib4_spec((n - 3) as nat)
+            + fib4_spec((n - 4) as nat),
+    }
+}
+
+proof fn lemma_fib4_monotonic_transitive(a: nat, b: nat)
+    requires
+        3 <= a <= b,
+    ensures
+        fib4_spec(a) <= fib4_spec(b),
+    decreases b - a,
+{
+    if a < b {
+        lemma_fib4_is_monotomic_after_inc_4(a);
+        lemma_fib4_monotonic_transitive((a + 1) as nat, b);
+    }
+}
+
+fn fib4(n: u32) -> (out: u64)
+    requires
+        n <= 70,
+    ensures
+        out == fib4_spec(n as nat),
+{
+    match n {
+        x if x <= 0 => return 0,
+        x if x == 1 => return 0,
+        x if x == 2 => return 2,
+        x if x == 3 => return 0,
+        _ => 0,
+    };
+
+    let mut x0 = 0;
+    let mut x1 = 0;
+    let mut x2 = 2;
+    let mut x3 = 0;
+    let mut i = 3;
+
+    while (i < n)
+        invariant
+            x3 == fib4_spec(i as nat),
+            x2 == fib4_spec((i - 1) as nat),
+            x1 == fib4_spec((i - 2) as nat),
+            x0 == fib4_spec((i - 3) as nat),
+            3 <= i <= n <= 70,
+        decreases n - i,
+    {
+        let x3p = x3;
+        proof {
+            lemma_fib4_n70_does_not_overflow();
+            lemma_fib4_monotonic_transitive((i + 1) as nat, 70);
+        }
+
+        x3 = x3 + x2 + x1 + x0;
+
+        x0 = x1;
+        x1 = x2;
+        x2 = x3p;
+        i += 1;
+    }
+    return x3;
+
+}
+
 // TODO: Put your solution (the specification, implementation, and proof) to the task here
+fn static_checks() {
+    reveal_with_fuel(fib4_spec, 8);
+
+    let x = fib4(5);
+    assert(x == 4);
+
+    let x = fib4(8);
+    assert(x == 28);
+}
 
 } // verus!
-fn main() {}
+fn main() {
+    assert!(fib4(5) == 4);
+    assert!(fib4(8) == 28);
+    assert!(fib4(10) == 104);
+    assert!(fib4(12) == 386);
+}
 
 /*
 ### VERUS END
